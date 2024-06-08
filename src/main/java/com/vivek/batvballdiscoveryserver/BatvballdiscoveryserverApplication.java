0b1e700a95d2b6c3dd861394c52497193cf90f11
VvePrime <vive021172@gmail.com>
2024-06-08 20:14:36 +0530

    initial commit

---
+++ src/main/java/com/vivek/batvballdiscoveryserver/BatvballdiscoveryserverApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.batvballdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatvballdiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatvballdiscoveryserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivek/batvballdiscoveryserver/BatvballdiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.batvballdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatvballdiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
